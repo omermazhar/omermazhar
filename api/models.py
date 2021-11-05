1fc1241e4d522813bfaf0f0c8c3d389bf50937c3
Emumba <omer.rana@emumba.com>
2021-11-05 14:26:01 +0500

    class based views

--- api/models.py
+++ api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Hero(models.Model):
     name = models.CharField(max_length=10)
     alias = models.CharField(max_length=10)
--- api/views.py
+++ api/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets
 from .serializers import HeroSerializer
 from .models import Hero
 from django.views.generic import ListView
-
 
 class HeroViewSet(ListView):
     queryset = Hero.objects.all().order_by('name')
