75d57a4fee6fe362e4d14ba35aff562dd46f4bfd
Emumba <omer.rana@emumba.com>
2021-11-05 14:15:54 +0500

    class based view

--- api/views.py
+++ api/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import HeroSerializer
 from .models import Hero
+from django.views.generic import ListView
 
 
-class HeroViewSet(viewsets.ModelViewSet):
+class HeroViewSet(ListView):
     queryset = Hero.objects.all().order_by('name')
     serializer_class = HeroSerializer
+    template_name = './list.html'
