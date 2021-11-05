4b4e2c796c1891823aeea077fe082d2e636fe5dc
Emumba <omer.rana@emumba.com>
2021-11-05 14:20:08 +0500

    function based view

--- api/views.py
+++ api/views.py
@@ -8,14 +8,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-
 class HeroViewSet(ListView):
     @api_view(['DELETE'])
     def delete_all_items(request):
         Hero.objects.all().delete()
         serializer_class = HeroSerializer
         return Response(serializer_class)
-    '''queryset = Hero.objects.all().order_by('name')
-    serializer_class = HeroSerializer
-    template_name = './list.html'
-    '''
